84d40b43f4519be91007f7c949e0e1aeece4f7ca
nadiand <nadetodobreva@gmail.com>
2024-11-22 15:57:35 +0100

    Finish board to list of tuple representation

--- board_to_graph.py
+++ board_to_graph.py
@@ -1,4 +1,3 @@
-import numpy as np
 import argparse
 
 
@@ -11,10 +10,25 @@ def make_base_connections(nr_cells, nr_dice_sides):
     return connections
 
 def add_ladders(connections, ladder_starts, ladder_ends):
-    pass
+    modified_connections = []
+    for con in connections:
+        if con[1] in ladder_starts:
+            ind = ladder_starts.index(con[1])
+            modified_connections.append((con[0], ladder_ends[ind]))
+        elif not (con[0] in ladder_starts):
+            modified_connections.append(con)
+    return modified_connections
 
 def add_snakes(connections, snake_starts, snake_ends):
-    pass
+    modified_connections = []
+    for con in connections:
+        if con[1] in snake_starts:
+            ind = snake_starts.index(con[1])
+            modified_connections.append((con[0], snake_ends[ind]))
+        elif not (con[0] in snake_starts):
+            modified_connections.append(con)
+
+    return modified_connections
 
 def connections_to_graph(connections):
     pass
@@ -33,6 +47,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     base_connections = make_base_connections(args.nr_cells, args.nr_dice_sides)
-    print(base_connections)
     connections = add_ladders(base_connections, args.ladder_starts, args.ladder_ends)
     final_connections = add_snakes(connections, args.snake_starts, args.snake_ends)
+    network = connections_to_graph(final_connections)
+
+    # Example usage (using the board from my notebook):
+    # python board_to_graph.py --nr_cells 9 --nr_dice_sides 4 --ladder_starts 2 --ladder_ends 6 --snake_starts 8 --snake_ends 3
